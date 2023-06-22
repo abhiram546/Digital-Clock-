b8a324b1129e24ca0752e54e62911741440b4500
ram <128051053+abhiram546@users.noreply.github.com>
2023-06-22 20:52:21 +0530

    Update Digital clock.py

--- Digital clock.py
+++ Digital clock.py
@@ -1,1 +1,18 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
 
+main = Tk()
+main.title("Digital Clock")
+
+def clock():
+    tick = strftime("%H:%M:%S %p")
+    clock_label.config(text=tick)
+    clock_label.after(1000, clock)
+
+clock_label = Label(main, font=('sans', 60), background='blue', foreground= 'black')
+
+clock_label.pack(anchor='center')
+
+clock()
+mainloop()
